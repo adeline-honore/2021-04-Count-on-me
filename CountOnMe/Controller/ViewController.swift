18657e158bc7e3b517267c1f571d3d9dec4acd54
adeline-honore <honore_adeline@yahoo.fr>
2021-05-02 22:58:03 +0200

    création du projet application responsive et envoi sur github

---
+++ CountOnMe/Controller/ViewController.swift
@@ -0,0 +1,167 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by Vincent Saluzzo on 29/03/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
+    
+    @IBOutlet weak var additionButton: UIButton!
+    @IBOutlet weak var substractionButton: UIButton!
+    @IBOutlet weak var multiplicationButton: UIButton!
+    @IBOutlet weak var divisionButton: UIButton!
+    
+    var elements: [String] {
+        return textView.text.split(separator: " ").map { "\($0)" }
+    }
+    
+    // Error check computed variables
+    var expressionIsCorrect: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveEnoughElement: Bool {
+        return elements.count >= 3
+    }
+    
+    var canAddOperator: Bool {
+        return elements.last != "+" && elements.last != "-"
+    }
+    
+    var expressionHaveResult: Bool {
+        return textView.text.firstIndex(of: "=") != nil
+    }
+    
+    // View Life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // View actions
+    @IBAction func tappedNumberButton(_ sender: UIButton) {
+        guard let numberText = sender.title(for: .normal) else {
+            return
+        }
+        
+        if expressionHaveResult {
+            textView.text = ""
+        }
+        
+        textView.text.append(numberText)
+    }
+    
+    
+    @IBAction func tappedOperandButton(_ sender: UIButton) {
+        
+        
+        
+        var witchOperand: String = ""
+        
+        switch sender {
+        case additionButton:
+            witchOperand = " + "
+        case substractionButton:
+            witchOperand =  " - "
+        case multiplicationButton:
+            witchOperand = " x "
+        case divisionButton:
+            witchOperand = " / "
+        default:
+            break
+        }
+        
+        if canAddOperator {
+            textView.text.append(witchOperand)
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func tappedAdditionButton(_ sender: UIButton) {
+        if canAddOperator {
+            textView.text.append(" + ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func tappedSubstractionButton(_ sender: UIButton) {
+        if canAddOperator {
+            textView.text.append(" - ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
+        if canAddOperator {
+            textView.text.append(" x ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func tappedDivisionButton(_ sender: UIButton) {
+        if canAddOperator {
+            textView.text.append(" / ")
+        } else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Un operateur est déja mis !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            self.present(alertVC, animated: true, completion: nil)
+        }
+    }
+
+    @IBAction func tappedEqualButton(_ sender: UIButton) {
+        guard expressionIsCorrect else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Entrez une expression correcte !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            return self.present(alertVC, animated: true, completion: nil)
+        }
+        
+        guard expressionHaveEnoughElement else {
+            let alertVC = UIAlertController(title: "Zéro!", message: "Démarrez un nouveau calcul !", preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            return self.present(alertVC, animated: true, completion: nil)
+        }
+        
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        
+        textView.text.append(" = \(operationsToReduce.first!)")
+    }
+
+}
+
